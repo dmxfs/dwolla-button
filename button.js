a24b5619b5f7f161c7f871715afbaa827413a045
Michael Schonfeld <dev@schonfeld.org>
2012-04-11 19:45:22 -0400

    brandon.r's changes

--- button.js
+++ button.js
@@ -9,103 +9,112 @@ var DwollaBtn = DwollaBtn || (function(){
         	// Append CSS
         	this.appendCSS();
 
+            // Style buttons
+            this.styleButtons();
+
         	// Bind buttons
         	this.registerButtons();
-
-        	// Style buttons
-        	this.styleButtons();
         },
         appendCSS: function() {
-        	// Create the <link> tag
-        	var cssFile = $('<link/>', {
-        		'rel': 'stylesheet',
-        		'type': 'text/css',
-        		'href': _args.cssURI
-        	});
+        	// Create the button <link> tag
+        	var cssFile = document.createElement('link');
+            cssFile.setAttribute('rel', 'stylesheet');
+            cssFile.setAttribute('type', 'text/css');
+            cssFile.setAttribute('href', _args.cssURI);
 
         	// Append the tag to the <head>
-        	$('head').append(cssFile);
+            document.getElementsByTagName('head')[0].appendChild(cssFile);
         },
         styleButtons: function() {
         	// Iterate thru every dwolla button on the page
-        	$('.dwolla_button').each(function() {
-        		var el = $(this),
-	        		styledBtn = el.clone(),
-        			amount = el.attr('data-amount');
+            var els = document.getElementsByClassName('dwolla_button');
 
-        		// Create the animated button's different elements
-        		var btnText = $('<span/>', { 'class': 'd-btn-text', 'html': el.html()}),
-        			btnSlideText = $('<span/>', { 'class': 'd-btn-slide-text', 'html': '$' + amount}),
-        			btnIcon = $('<span/>', { 'class': 'd-btn-icon-right', 'html': '<span></span>'});
+            for(i=0 ; i<els.length ; i++) {
+                var btn = els[i],
+                    amount = btn.getAttribute('data-amount');
 
-        		// Append the various elements to the styled button element
-        		styledBtn
-        			.empty()
-        			.append(btnText)
-        			.append(btnSlideText)
-        			.append(btnIcon)
-        			.addClass('d-btn')
-        			.unbind('mouseover mouseout') // Clear out any old listeners
-        			.bind({ // Register hover listeners
-        				mouseover: function() {
-        					// Expand button to fit price text
-        					styledBtn
-        						.css({'padding-right': 80 + (amount.length * 15)})
-        						.find('.d-btn-slide-text').width(amount.length * 15);
-        				},
-        				mouseout: function() {
-        					styledBtn
-        						.css({'padding-right': 80})
-        						.find('.d-btn-slide-text').width(0);
-        				}
-        			})
+                // Wrap link text in a <span>
+                var btnText = document.createElement('span');
+                btnText.setAttribute('class', 'd-btn-text');
+                btnText.appendChild( document.createTextNode(btn.firstChild.nodeValue) );
+                btn.replaceChild(btnText, btn.firstChild)
 
-        		// Finally, replace the old button with 
-        		// the new styled one
-        		el.replaceWith(styledBtn);
-        	})
+                // Add the arrow icon
+                var btnIcon = document.createElement('span');
+                btnIcon.setAttribute('class', 'd-btn-icon');
+                btn.appendChild(btnIcon);
+
+                // Add the d-btn class
+                btn.className += " d-btn";
+            }
         },
         registerButtons: function() {
-        	$('.dwolla_button')
-        	.unbind('click.DwollaBtn') // Clear out any old listeners
-        	.live('click.DwollaBtn', function(e) { // Register our listener in our namespace
-        		// Avoid any unwanted actions from the
-        		// button element
-        		e.preventDefault();
+            // Helper function to determine whether
+            // a dwolla button was clicked
+            var isDBtn = function(el, parent_el) {
+                var check = function(e) { return (e.className.indexOf('d-btn') !== -1) && e.nodeName == "A" }
 
-        		// Create a dynamic form
-        		var el = $(this),
-        			form = $('<form/>', {
-        				'method': 'POST',
-        				'action': _args.dwollaPayAction
-	        		});
+                if (check(el)) {
+                    return el;
+                }
+                else if (check(parent_el)) {
+                    return parent_el;
+                }
 
-	        	// Create and append inputs
-	        	var inputs = {
-		        		destinationId: el.attr('data-dest'),
-		        		amount: el.attr('data-amount'),
-		        		shipping: el.attr('data-shipping'),
-		        		tax: el.attr('data-tax'),
-		        		name: el.attr('data-name'),
-		        		desc: el.attr('data-desc'),
-		        		redirect: el.attr('href'),
-		        		key: el.attr('data-key')
-		        	};
-	        	for(key in inputs) {
-	        		var input = $('<input/>', {
-	        			'type'	: 'hidden',
-	        			'name'	: key,
-	        			'value'	: inputs[key]
-	        		});
-	        		form.append(input);
-	        	}
+                return false;
+            }
 
-	        	// Put the form in the body and submit
-	        	$('body').append(form);
-	        	form.submit();
+            // Mimic jQuery's .live method
+            document.onclick = function(e) {
+                // Check both the clicked element
+                // and its parent, since we have a 1-level
+                // nested button
+                var click_el = e.target,
+                    parent_el = e.target.parentNode,
+                    el;
 
-	        	return false;
-        	});
+                if (el = isDBtn(click_el, parent_el)) {
+                    // Avoid any unwanted actions from the
+                    // button element
+                    e.preventDefault();
+
+                    // Create a dynamic form
+                    var form = document.createElement("form");
+
+                    // Config the form
+                    form.setAttribute('method', 'POST');
+                    form.setAttribute('action', _args.dwollaPayAction);
+
+                    // Create and append inputs
+                    var inputs = {
+                            destinationId: el.getAttribute('data-dest'),
+                            amount: el.getAttribute('data-amount'),
+                            shipping: el.getAttribute('data-shipping'),
+                            tax: el.getAttribute('data-tax'),
+                            name: el.getAttribute('data-name'),
+                            desc: el.getAttribute('data-desc'),
+                            redirect: el.getAttribute('href'),
+                            key: el.getAttribute('data-key')
+                        };
+                    for(key in inputs) {
+                        var input = document.createElement("input");
+
+                        input.setAttribute('type', 'hidden');
+                        input.setAttribute('name', key);
+                        input.setAttribute('value', inputs[key]);
+
+                        form.appendChild(input);
+                    }
+
+                    // Put the form in the body and submit
+                    document.getElementsByTagName('body')[0].appendChild(form);
+                    //form.submit();
+                    alert('a');
+
+                    // Don't continue to the href
+                    return false;                    
+                }
+            };
         }
     };
 }());
