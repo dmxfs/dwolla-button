854c2b7ccb60c43dbed0e673bc45ed055105d1df
troyblank <troy@troyblank.com>
2014-06-30 16:11:12 -0500

    made zombie integration tests self sufficient

--- Tests/test.js
+++ Tests/test.js
@@ -1,30 +1,65 @@
-var Browser = require("zombie")
-	, assert = require("assert")
-	, path = require("path")
-	, folder = path.dirname(__dirname)
-	, browser = new Browser()
-	;
+var express = require('express');
+var Browser = require('zombie');
+var assert = require('assert');
+var path = require('path');
+var folder = path.dirname(__dirname);
 
-browser.visit("file:/" + folder + "/index.html", function () {	
-	// Check that the page loaded OK
-	assert.ok(browser.success, 'Test page load failed');
+var browser = new Browser();
 
-	// Check that the button has the correct
-	assert.strictEqual(browser.queryAll(".d-btn").length, 4, "Couldn't find 4 rendered buttons.");
+//-------------------------------------------------------------------------------------------------
+//SERVE EXAMPLE HTML
+//-------------------------------------------------------------------------------------------------
+var server = {
 
-	// Check that the CSS stylesheet has
-	// been injected
-	assert.strictEqual(browser.queryAll("link[href='https://www.dwolla.com/content/button.min.css']").length, 1, "Couldn't find Dwolla's stylesheet");
+    app: express(),
+    server: null,
+    files: [],
 
-	// Follow the first button
-	browser
-		.clickLink("A button", function() {
-			// Check that the page loaded OK
-			assert.ok(browser.success, 'Checkout load failed');
+    initialize: function() {
+        server.urlConfs();
+        server.startWebServer();
+    },
 
-			// Make sure the result URL is OK
-			assert.notStrictEqual(browser.location._url.href.indexOf("https://www.dwolla.com/payment"), -1, 'Error redirect occured');
+    startWebServer: function() {
+        server.server = server.app.listen(8083);
+        console.log('Serving example at http://localhost:8083/buttons.html.');
+    },
 
-			console.log('Test completed successfully');
-		});
+    example:function(req, res) {
+        res.render('buttons.html');
+    },
+
+    urlConfs: function() {
+        server.app.use(express.static(path.join(__dirname +'/../examples')));
+    }
+}
+
+server.initialize();
+
+
+//-------------------------------------------------------------------------------------------------
+//RUN ZOMBIE TEST
+//-------------------------------------------------------------------------------------------------
+browser.visit('http://localhost:8083/buttons.html', function () {   
+    //Check that the page loaded OK
+    assert.ok(browser.success, 'Test page load failed');
+
+    // Check that the button has the correct
+    assert.strictEqual(browser.queryAll('.d-btn').length, 4, 'Couldn\'t find 4 rendered buttons.');
+
+    //Check that the CSS stylesheet has been injected
+    assert.strictEqual(browser.queryAll('link[href="https://www.dwolla.com/content/button.min.css"]').length, 1, 'Couldn\'t find Dwolla\'s stylesheet');
+
+    // Follow the first button
+    browser.clickLink('Simple Payment Button', function() {
+        // Check that the page loaded OK
+        assert.ok(browser.success, 'Checkout load failed');
+
+        // Make sure the result URL is OK
+        assert.notStrictEqual(browser.location.pathname.indexOf('/Payment/Login'), -1, 'Error redirect occured');
+
+        console.log('Test completed successfully.');
+        server.server.close();
+        console.log('Shut down server.')
+    });
 });
